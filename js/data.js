1d889c3a99663b3e3afb074e6cb0243f2352ddfb
Jordi Vilaplana <gatasuna@gmail.com>
2022-06-25 02:27:17 +0200

    Added latest game data.

---
+++ js/data.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// JS global variables
+let data = {
+    gameCount: 3,
+    completedScenarios: 2,
+    lastGame: "06/24/2022",
+    games22: [0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0],
+    gamesDates: ["05/27/2022", "06/17/2022", "06/24/2022"],
+    totalScenarios: 75,
+    gameBought: "05/12/2022"
+};
